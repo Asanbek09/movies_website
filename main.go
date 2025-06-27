2d8aef24d52f44531219e6c96b051cd26180b298
Asanbek09 <samudinua@gmail.com>
2025-06-27 14:47:18 +0600

    Первые шаги по проекту

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	const addr = "localhost:8082"
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+
 }
