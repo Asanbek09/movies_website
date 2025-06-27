8e7d538271dd363fdbd2117e15997cb9cba897ed
Asanbek09 <samudinua@gmail.com>
2025-06-27 16:34:58 +0600

    Добавляем логирование к нашему проекту

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"log"
+	"movies/logger"
 	"net/http"
 )
 
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movies.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize logger $v", err)
+	}
+	defer logInstance.Close()
+
+	return logInstance
+}
+
 func main() {
+
+	logInstance := initializeLogger()
+
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = "localhost:8082"
@@ -13,6 +27,7 @@ func main() {
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed", err)
 	}
 
 }
