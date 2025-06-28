6d0f6833f2460f828fc0e76ee455bfdba16b55bb
Asanbek09 <samudinua@gmail.com>
2025-06-28 19:23:35 +0600

    Добавляем еще один Route Handler к проекту

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,29 +8,47 @@ import (
 
 type MovieHandler struct{}
 
+func (h *MovieHandler) writeJSONResponse(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
 func (h *MovieHandler) GetTopMovies(w http.ResponseWriter, r *http.Request) {
-	movies := []models.Movie {
+	movies := []models.Movie{
 		{
-			ID: 1,
-			TMDB_ID: 182,
+			ID:          1,
+			TMDB_ID:     182,
 			ReleaseYear: 2022,
-			Genres: []models.Genre{{ID: 1, Name: "Thriller"}},
-			Keywords: []string{},
-			Casting: []models.Actor{{ID: 1, FirstName: "Oscar"}},
+			Genres:      []models.Genre{{ID: 1, Name: "Thriller"}},
+			Keywords:    []string{},
+			Casting:     []models.Actor{{ID: 1, FirstName: "Oscar"}},
 		},
 		{
-			ID: 2,
-			TMDB_ID: 189,
+			ID:          2,
+			TMDB_ID:     189,
 			ReleaseYear: 1996,
-			Genres: []models.Genre{{ID: 3, Name: "Green mile"}},
-			Keywords: []string{},
-			Casting: []models.Actor{{ID: 3, FirstName: "Koffee"}},
+			Genres:      []models.Genre{{ID: 3, Name: "Green mile"}},
+			Keywords:    []string{},
+			Casting:     []models.Actor{{ID: 3, FirstName: "Koffee"}},
 		},
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(movies); err != nil {
-		// todo: log error
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	h.writeJSONResponse(w, movies)
+}
+
+func (h *MovieHandler) GetRandomMovies(w http.ResponseWriter, r *http.Request) {
+	movies := []models.Movie{
+		{
+			ID:          4,
+			TMDB_ID:     165,
+			ReleaseYear: 2021,
+			Genres:      []models.Genre{{ID: 5, Name: "Alien"}},
+			Keywords:    []string{},
+			Casting:     []models.Actor{{ID: 7, FirstName: "Norris"}},
+		},
 	}
+
+	h.writeJSONResponse(w, movies)
 }
--- main.go
+++ main.go
@@ -23,6 +23,8 @@ func main() {
 
 	movieHandler := handlers.MovieHandler{}
 	http.HandleFunc("/api/movies/top", movieHandler.GetTopMovies)
+	http.HandleFunc("/api/movies/random", movieHandler.GetRandomMovies)
+
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = "localhost:8082"
