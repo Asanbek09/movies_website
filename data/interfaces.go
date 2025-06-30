90ede201426047874dd495f7268c4a0e4716b9ac
Asanbek09 <samudinua@gmail.com>
2025-06-30 17:01:49 +0600

    Создаем интерфейс для movie

---
+++ data/interfaces.go
@@ -0,0 +1,11 @@
+package data
+
+import "movies/models"
+
+type MovirStorage interface {
+	GetTopMovies() ([]models.Movie, error)
+	GetRandomMovies() ([]models.Movie, error)
+	GetMovieById(id int) (models.Movie, error)
+	SearchMovies(name string) ([]models.Movie, error)
+	GetAllGenres() ([]models.Genre, error)
+}
