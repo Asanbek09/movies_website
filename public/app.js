0a1ade48e44e469e3e0412e3230d31e15e33fe4a
Asanbek09 <samudinua@gmail.com>
2025-07-16 22:52:36 +0600

    Реализуем компонент MovieDetails

--- public/app.js
+++ public/app.js
@@ -1,9 +1,11 @@
 import { API } from "./services/API.js";
 import { HomePage } from "./components/HomePage.js";
 import './components/AnimatedLoading.js';
+import { MovieDetailsPage } from "./components/MovieDetailsPage.js";
 
 window.addEventListener("DOMContentLoaded", event => {
     document.querySelector("main").appendChild(new HomePage())
+    document.querySelector("main").appendChild(new MovieDetailsPage())
 });
 
 window.app = {
