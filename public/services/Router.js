767cd6d796033a38d2507e31c5bd2373ce32c665
Asanbek09 <samudinua@gmail.com>
2025-07-18 05:48:01 +0600

    Продолжаем работу с Router.js

--- public/services/Router.js
+++ public/services/Router.js
@@ -1,3 +1,5 @@
+import { routes } from "./Routes";
+
 const Router = {
     init: () =>{
         window.addEventListener("popstate", () => {
@@ -10,6 +12,31 @@ const Router = {
     go: (route, addToHistory=true) => {
         if (addToHistory) {
             history.pushState(null, "", route)
+        }
+        let pageElement = null
+
+        const routePath = route.includes('?') ? route.split("?")[0] : route;
+
+        for (const r of routes) {
+            if (typeof r.path == "string" && r.path === routePath) {
+                pageElement = new r.component();
+                break;
+            } else if (r.path instanceof RegExp) {
+                const match = r.path.exec(route);
+                if (match) {
+                    pageElement = new r.component();
+                    const params = match.slice(1);
+                    pageElement.params = params;
+                }
+            }
+        }
+
+        if (pageElement == null) {
+            pageElement = document.createElement("h1")
+            pageElement.textContent = "Page not found"
+        } else {
+            document.querySelector("main").innerHTML ="";
+            document.querySelector("main").appendChild(pageElement);
         }
     }
 }
